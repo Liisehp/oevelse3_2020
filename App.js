e58367450317736f035ff38af76fa44d96640abd
Liisehp <lise.h.pedersen@hotmail.com>
2020-09-23 11:19:18 +0200

    test

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import HomeScreen from "./components/HomeScreen";
+import { createAppContainer } from 'react-navigation';
+import SettingStack from "./components/StackNavigation";
+import {Image,StyleSheet} from "react-native-web";
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tabIcon: {
+    width: 32,
+    height: 32,
   },
 });
+
+// Denne TabNavigator holder styr på det yderste niveau af navigation i appen.
+const TabNavigator = createBottomTabNavigator(
+    {
+      /*Tilføj routes*/
+      Home: {
+        /*HVilket view skal loades*/
+        screen: HomeScreen,
+        /*Instillinger til navigation*/
+        navigationOptions: {
+          /*Navn*/
+          tabBarLabel:"Home Page",
+          /*Ikon*/
+          tabBarIcon: ({ tintColor }) => (
+              <Entypo name="home" size={24} color={tintColor} />
+          )
+        },
+      },
+      /*Navn på Route*/
+      Settings: {
+        screen: SettingStack,
+        navigationOptions: {
+          tabBarLabel:"Settings",
+          tabBarIcon: ({ tintColor }) => (
+              <Ionicons name="ios-settings" size={24} color={tintColor} />
+          )
+        },
+      },
+    },
+    /*Generelle label indstillinger*/
+    {
+      tabBarOptions: {
+        showIcon:true,
+        labelStyle: {
+          fontSize: 15,
+        },
+        activeTintColor: 'blue',
+        inactiveTintColor: 'gray',
+        size:40
+      }
+    }
+
+)
+
+
+export default createAppContainer(TabNavigator)
---
+++ components/HomeScreen.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class HomeScreen extends Component {
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.text}>HomeScreen</Text>
+            </View>
+        );
+    };
+
+}
+const styles = StyleSheet.create({
+    container: {
+        borderColor: 'red',
+        borderWidth: 20,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    text: {
+        fontSize: 20,
+    },
+});
